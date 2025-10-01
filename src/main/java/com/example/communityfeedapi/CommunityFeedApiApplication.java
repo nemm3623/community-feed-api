83129b9e4871490d53cb9f5af0147616c364ee0f
6zeropk <nemm3623@naver.com>
2025-10-01 13:24:37 +0900

    Initial commit

---
+++ src/main/java/com/example/communityfeedapi/CommunityFeedApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.communityfeedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityFeedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityFeedApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/communityfeedapi/CommunityFeedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.communityfeedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityFeedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
